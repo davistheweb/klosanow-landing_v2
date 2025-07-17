c7c7e41ff6402f105c1d39f6cb2e3c58e8a07459
davistheweb <josiahdave001@gmail.com>
2025-07-17 07:55:01 +0100

    added accordin

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -2,10 +2,11 @@ import Image from "next/image";
 import React from "react";
 import { contactImg } from "@/assets";
 import { ContactForm } from "@/components/contact/ContactForm";
+import { FAQ } from "@/components/contact/FAQ";
 
 const Contact: React.FC = () => {
   return (
-    <div className="relative mt-24 overflow-x-hidden pb-10">
+    <div className="relative mt-24 overflow-x-hidden pb-5">
       <div className="p-5 sm:p-20">
         <div className="bg-neutral-xmd py-20 rounded-xl w-full">
           <div className="flex justify-center items-center">
@@ -22,6 +23,15 @@ const Contact: React.FC = () => {
               <Image src={contactImg} alt="contact_img" width={300} />
             </div>
           </div>
+        </div>
+      </div>
+      {/* Accordin FAQS */}
+      <div className="flex flex-col justify-center items-center w-full bg-[#f4f4f4] p-5 sm:p-20">
+        <div className="flex justify-center items-center">
+          <h1 className="font-main text-2xl lg:text-4xl font-semibold">FAQS</h1>
+        </div>
+        <div className="w-full flex justify-center mt-4">
+          <FAQ />
         </div>
       </div>
     </div>
--- src/components/contact/FAQ.tsx
+++ src/components/contact/FAQ.tsx
@@ -1,5 +1,28 @@
 import React from "react";
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "@/components/ui/accordion";
+import { faqItems } from "@/data";
 
-export const FAQ: React.FC = () => {
-  return <div>FAQ</div>;
-};
+export const FAQ: React.FC = () => (
+  <Accordion
+    type="single"
+    collapsible
+    className="w-[430px] md:w-[500px] lg:w-[900px] bg-white p-10"
+    defaultValue="item-1"
+  >
+    {faqItems.map(({ title, text }, _i) => (
+      <AccordionItem key={_i} value={`"item-${_i+1}"`} className="w-full border-gray-300">
+        <AccordionTrigger className="w-full font-main font-bold text-[16px] lg:text-xl cursor-pointer">
+          {title}
+        </AccordionTrigger>
+        <AccordionContent className="flex flex-col gap-4 text-balance">
+          <p className="font-accent text-[16px] lg:text-[18px]">{text}</p>
+        </AccordionContent>
+      </AccordionItem>
+    ))}
+  </Accordion>
+);
